b9e03d93290a44cd117326ce2d639b590eff2e52
MyeonghoonNam <codenmh0822@naver.com>
2024-08-06 22:14:30 +0900

    feat : input exception handling

---
+++ src/constants/validate.js
@@ -0,0 +1,6 @@
+const INPUT_VALIDATION_ERRORS = {
+  INVALID_PLAYER_COUNT:
+    "The number of players must be an integer greater than or equal to 1",
+};
+
+module.exports = { INPUT_VALIDATION_ERRORS };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 const intro = require("./intro");
+const cli = require("./cli");
+const game = require("./game");
 
 async function main() {
-  intro();
+  try {
+    intro();
+    const answers = await cli();
+    game(answers);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 main();
