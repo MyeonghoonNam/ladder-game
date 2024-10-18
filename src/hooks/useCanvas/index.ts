e3643a914719ccef5054e3994eda269733ef37c7
MyeonghoonNam <codenmh0822@naver.com>
2024-10-18 18:45:57 +0900

    feat #6 : useCanvas custom hook 구현

---
+++ src/hooks/useCanvas/index.ts
@@ -0,0 +1,33 @@
+import { useCallback, useState } from 'react';
+
+interface Props {
+  draw?: (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) => void;
+}
+
+const useCanvas = ({ draw }: Props) => {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const canvasRef = useCallback(
+    (node: HTMLCanvasElement) => {
+      if (node) {
+        const ctx = node.getContext('2d');
+        if (!ctx) return;
+
+        const { width, height } = node.getBoundingClientRect();
+
+        setWidth(width);
+        setHeight(height);
+
+        if (draw) {
+          draw(node, ctx);
+        }
+      }
+    },
+    [width, height]
+  );
+
+  return { canvasRef, width, height };
+};
+
+export default useCanvas;
