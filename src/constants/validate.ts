317fadda3bdfff7ca2b0c1b1002e51321cf633dd
MyeonghoonNam <codenmh0822@naver.com>
2024-11-01 19:32:52 +0900

    remove #9 : 기존 figlet 관련 intro 제거

--- src/constants/validate.ts
+++
@@ -1,4 +0,0 @@
-export const INPUT_VALIDATION_ERRORS = {
-  INVALID_PLAYER_COUNT: 'The number of players must be from 2 to 10',
-  INVALID_POINT_VALUE: 'The string length of start and end point from 1 to 10 characters',
-};
