6416d84cb623b600a563fa0f6c13ee278c77defe
MyeonghoonNam <codenmh0822@naver.com>
2024-11-12 22:10:00 +0900

    feat #13 : 선택된 경로 추적 기능 구현

--- src/reducers/game/index.ts
+++ src/reducers/game/index.ts
@@ -1,55 +1,41 @@
 import { LADDER_FOOT_STOOLS } from 'constants/ladder';
 import { type Ladder } from 'models';
 
-type Action = { type: 'start_game'; width: number; height: number };
+type Action =
+  | { type: 'start_game'; width: number; height: number }
+  | { type: 'init_game'; width: number; height: number }
+  | { type: 'start_game'; width: number; height: number };
 
 export const initialState: Ladder = [[]];
 
 export function gameReducer(state: Ladder, action: Action) {
-  switch (action.type) {
-    case 'start_game': {
-      const { width, height } = action;
-
-      do {
-        reset(width, height);
-        randomFill(width, height);
-      } while (!analyze(width, height));
-
-      return state;
-    }
-
-    default: {
-      new Error();
-    }
-  }
-
   /**
    * ladder structure initialization function without footStool
    */
-  function reset(width: number, height: number) {
+  const reset = (width: number, height: number) => {
     state = Array.from(new Array(height), () => new Array(width).fill(''));
-  }
+  };
 
   /**
    * function for getting the total number of random footStool
    * @returns total number of random footStool
    */
-  function randomFootStoolCount(width: number, height: number) {
+  const randomFootStoolCount = (width: number, height: number) => {
     return Math.floor(Math.random() * (width * height)) + 1;
-  }
+  };
 
   /**
    * function for getting only one footStool of LADDER_FOOT_STOOLS
    * @returns one random footStool
    */
-  function randomFootStool() {
+  const randomFootStool = () => {
     return LADDER_FOOT_STOOLS[Math.floor(Math.random() * LADDER_FOOT_STOOLS.length)];
-  }
+  };
 
   /**
    * function to fill an empty ladder with random LADDER_FOOT_STOOLS
    */
-  function randomFill(width: number, height: number) {
+  const randomFill = (width: number, height: number) => {
     let totalFootStoolCount = randomFootStoolCount(width, height);
     let count = 0;
 
@@ -60,13 +46,13 @@ export function gameReducer(state: Ladder, action: Action) {
       state[y][x] = randomFootStool();
       count += 1;
     }
-  }
+  };
 
   /**
    * funtion to validate correct ladder structure
    * @returns validate the correct ladder structure
    */
-  function analyze(width: number, height: number) {
+  const analyze = (width: number, height: number) => {
     for (let i = 0; i < height; i++) {
       for (let j = 0; j < width - 1; j++) {
         if (state[i][j] === LADDER_FOOT_STOOLS[0] && state[i][j + 1] === LADDER_FOOT_STOOLS[0]) {
@@ -84,5 +70,26 @@ export function gameReducer(state: Ladder, action: Action) {
     }
 
     return true;
+  };
+
+  switch (action.type) {
+    case 'init_game': {
+      return;
+    }
+
+    case 'start_game': {
+      const { width, height } = action;
+
+      do {
+        reset(width, height);
+        randomFill(width, height);
+      } while (!analyze(width, height));
+
+      return state;
+    }
+
+    default: {
+      new Error();
+    }
   }
 }
