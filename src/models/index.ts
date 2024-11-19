d5ca5862a815026a0c854dff078c5de887ef41b6
MyeonghoonNam <codenmh0822@naver.com>
2024-11-19 23:31:03 +0900

    refactor #18 : model에 따른 type 분리

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,19 +1,2 @@
-export type Ladder = LadderFootStool[][];
-export type LadderFootStool = '---' | '\\-\\' | '/-/' | '';
-export interface LadderContactPoint {
-  coords: {
-    x: number;
-    y: number;
-  };
-  verticalLineIdx: number;
-  connectedPointCoords: {
-    x: number;
-    y: number;
-  };
-  contactedVerticalLineIdx: number;
-}
-
-export interface LadderSelectedInput {
-  selectedInputLineIdx: number;
-  selectedInputIdx: number;
-}
+export * from './common';
+export * from './ladder';
