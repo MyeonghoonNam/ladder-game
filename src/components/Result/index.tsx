2c5eb6610a6931939a4b2c45428c48cbc2d70e49
MyeonghoonNam <codenmh0822@naver.com>
2025-01-29 16:24:44 +0900

    feat #23 : Result 컴포넌트 스타일 구현

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -1,20 +1,23 @@
-import { type LadderGameResult } from 'models';
+import { LadderGameResult } from 'models';
+import * as Styled from './styled';
 
 interface ResultProps {
   result: LadderGameResult[];
 }
 
 export default function Result({ result }: ResultProps) {
-  console.log(result);
   return (
-    <div>
-      {result.map(({ start, end }, idx) => (
-        <div key={`key_${idx}`}>
-          <span>{start}</span>
-          <span>{'=>'}</span>
-          <span>{end}</span>
-        </div>
-      ))}
-    </div>
+    <Styled.ResultContainer>
+      <Styled.ResultTitle>게임 결과</Styled.ResultTitle>
+      <Styled.ResultList>
+        {result.map(({ start, end }, idx) => (
+          <Styled.ResultItem key={`key_${idx}`}>
+            <Styled.StartPoint>{start}</Styled.StartPoint>
+            <Styled.RightArrow />
+            <Styled.EndPoint>{end}</Styled.EndPoint>
+          </Styled.ResultItem>
+        ))}
+      </Styled.ResultList>
+    </Styled.ResultContainer>
   );
 }
---
+++ src/components/Result/styled.ts
@@ -0,0 +1,53 @@
+import { BiRightArrowAlt } from 'react-icons/bi';
+import styled from '@emotion/styled';
+
+export const ResultContainer = styled.div`
+  padding: ${({ theme }) => theme.spacing.large};
+  background-color: ${({ theme }) => theme.colors.neutral10};
+  border-radius: ${({ theme }) => theme.radius.medium};
+  box-shadow: ${({ theme }) => theme.shadows.small};
+`;
+
+export const ResultTitle = styled.h2`
+  ${({ theme }) => theme.typography.heading3};
+  color: ${({ theme }) => theme.colors.brown40};
+  margin-bottom: ${({ theme }) => theme.spacing.medium};
+  text-align: center;
+`;
+
+export const ResultList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.medium};
+`;
+
+export const ResultItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: ${({ theme }) => theme.spacing.medium};
+  padding: ${({ theme }) => theme.spacing.medium};
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: ${({ theme }) => theme.radius.small};
+`;
+
+export const StartPoint = styled.span`
+  ${({ theme }) => theme.typography.bodyMedium};
+  color: ${({ theme }) => theme.colors.brown30};
+  font-weight: bold;
+  padding: ${({ theme }) => theme.spacing.small};
+  background-color: ${({ theme }) => theme.colors.neutral50};
+  border-radius: ${({ theme }) => theme.radius.small};
+  cursor: default;
+`;
+
+export const EndPoint = styled(StartPoint)`
+  color: ${({ theme }) => theme.colors.brown50};
+  cursor: default;
+`;
+
+export const RightArrow = styled(BiRightArrowAlt)`
+  ${({ theme }) => theme.typography.bodyLarge};
+  color: ${({ theme }) => theme.colors.neutral60};
+  cursor: default;
+`;
