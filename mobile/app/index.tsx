e1c1662e4e47bf7f0126f91d523bfb92ee697634
MyeonghoonNam <codenmh0822@naver.com>
2025-02-07 21:32:37 +0900

    feat #29 : Webview를 통한 에뮬레이터 렌더링

---
+++ mobile/app/index.tsx
@@ -0,0 +1,19 @@
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView style={styles.webview} source={{ uri: 'https://laddergame.vercel.app/' }} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
