4854be05701b491d2fe7cba3ed3c5cf928eb2290
MyeonghoonNam <codenmh0822@naver.com>
2025-02-07 18:34:03 +0900

    hotfix : 불필요 속성 제거

--- webpack.common.cjs
+++ webpack.common.cjs
@@ -8,7 +8,6 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
