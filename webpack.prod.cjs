8308719f37a8950aa54b1ab609de2bffffda386a
MyeonghoonNam <codenmh0822@naver.com>
2025-02-07 18:14:56 +0900

    hotfix : 프로덕션 빌드 설정 수정

--- webpack.prod.cjs
+++ webpack.prod.cjs
@@ -4,4 +4,7 @@ const common = require('./webpack.common.cjs');
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'hidden-source-map',
+  performance: {
+    hints: false, // 성능 경고 비활성화
+  },
 });
